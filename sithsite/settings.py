af8e21245c01095d71f3fc17f941ca648a327c9c
denvyugo <denvyugo@gmail.com>
2019-12-05 23:52:33 +0300

    initial commit

--- sithsite/settings.py
+++ sithsite/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate django_heroku
 django_heroku.settings(locals())
