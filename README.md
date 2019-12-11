edff10be77cc52c56aa598650731ecd0d0013dee
denvyugo <denvyugo@gmail.com>
2019-12-11 21:44:31 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # shadow_recruit
-Django app
+Django app for a little quiz.
+
+Application for each quiz form database random choose a few questions (number of questions set as
+constant in view).
--- sithsite/settings.py
+++ sithsite/settings.py
@@ -95,15 +95,15 @@ WSGI_APPLICATION = 'sithsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if env_config['SECRET_KEY']:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+else:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
