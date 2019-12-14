e704a57288d97bd580023f97be178ddacbaeb5a0
denvyugo <denvyugo@gmail.com>
2019-12-14 15:09:22 +0300

    select recruit to master

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -6,12 +6,9 @@ class RecruitCreateForm(forms.ModelForm):
     """form for recruit profile"""
     class Meta:
         model = Recruit
-        fields = '__all__'
+        exclude = ('master',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-
-
-# class SithListForm(forms.ModelForm):
