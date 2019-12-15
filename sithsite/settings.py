4aecb318f2cf9968845b7084003676de309f0106
denvyugo <denvyugo@gmail.com>
2019-12-15 14:31:50 +0300

    select recruit to master

--- sithsite/settings.py
+++ sithsite/settings.py
@@ -95,14 +95,15 @@ WSGI_APPLICATION = 'sithsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-if env_config['SECRET_KEY']:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
+}
+
+if not env_config['SECRET_KEY']:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
